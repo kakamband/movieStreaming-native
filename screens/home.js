67c086fd27089f603684e6bb856dae3ba3d31c77
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-07-09 15:10:25 -0300

    screenview Home

--- screens/home.js
+++ screens/home.js
@@ -1,14 +1,87 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useRef, useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  StatusBar,
+  ImageBackground,
+  TextInput,
+  FlatList,
+  Platform,
+} from "react-native";
+import Carousel from "react-native-anchor-carousel";
+import { FontAwesome5, Feather, MaterialIcons } from "@expo/vector-icons";
 
 // import { Container } from './styles';
 
 const Home = () => {
+  const [backgound, setBackground] = useState({
+    uri:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMwtU95JygcmCq4e3XPpL9W0ATjGFoQCQm16dbpIp7BM36B7lH",
+    name: "Avengers: End Game",
+    stat: "2019 - Action/Sci-fi - 3h",
+    desc:
+      "Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.",
+  });
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.carouselContentContainer}>
+        <View style={{ ...StyleSheet.absoluteFill, backgroundColor: "#000" }}>
+          <ImageBackground
+            source={{ uri: backgound.uri }}
+            style={styles.ImageBg}
+          >
+            <View style={styles.SearchBoxContainer}>
+              <TextInput
+                placeholder="Search Movies"
+                placeholderTextColor="#666"
+                style={styles.SearchBox}
+              />
+            </View>
+          </ImageBackground>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  carouselContentContainer: {
+    flex: 1,
+    backgroundColor: "#000",
+    height: 720,
+  },
+  ImageBg: {
+    flex: 1,
+    height: null,
+    width: null,
+    opacity: 1,
+    justifyContent: "flex-start",
+    paddingTop: Platform.OS === "ios" ? 30 : 10,
+  },
+  SearchBoxContainer: {
+    backgroundColor: "#fff",
+    elevation: 10,
+    borderRadius: 4,
+    marginVertical: 10,
+    width: "95%",
+    flexDirection: "row",
+    alignSelf: "center",
+  },
+  SearchBox: {
+    padding: 12,
+    paddingLeft: 20,
+    fontSize: 16,
+  },
+});
 
 export default Home;
