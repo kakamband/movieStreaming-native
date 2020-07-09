27502914f708988fa90e97a2808b70641156cae0
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-07-09 14:38:21 -0300

    Initial routes

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import Home from "./screens/home";
+import Profile from "./screens/profile";
+import Recent from "./screens/recents";
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        activeColor="#02ad94"
+        inactiveColor="#dedede"
+        style={{ backgroundColor: "#000" }}
+        barStyle={{ backgroundColor: "#0f0f0f", padding: 4 }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={28} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="camera-metering-spot"
+                color={color}
+                size={28}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Recent"
+          component={Recent}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={28} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/recents.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+const Recents = () => {
+  return (
+    <View>
+      <Text>Recents</Text>
+    </View>
+  );
+};
+
+export default Recents;
